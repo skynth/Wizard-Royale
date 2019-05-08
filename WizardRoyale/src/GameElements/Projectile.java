7f1e0a5d283e1293451b5050d3919043b01ac991
julianng-thow-hing <julianng-thow-hing@10.16.89.200>
2019-05-08 11:16:21 -0700

    I add stuff to the projectile class

--- WizardRoyale/src/GameElements/Projectile.java
+++ WizardRoyale/src/GameElements/Projectile.java
@@ -1,6 +1,39 @@
 package GameElements;
 
-public class Projectile {
+import java.awt.Graphics;
+
+import Main.ID;
+
+public class Projectile extends GameObject{
 	
-	private String type; 
+	private String type;
+	private int mouseX, mouseY;
+
+	
+	public Projectile(String t, int x, int y, ID id, int mouseX, int mouseY) {
+		super(x, y, id);
+		type = t;
+		this.mouseX = mouseX;
+		this.mouseY = mouseY;
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	public String getType() {
+		return type;
+	}
+	
+
+	
+
 }

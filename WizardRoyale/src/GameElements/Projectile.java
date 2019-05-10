ca49ccfd23a528ba279d664082e6431cde849c9b
julianng-thow-hing <julianng-thow-hing@10.0.0.22>
2019-05-09 19:06:57 -0700

    worked on the projectile class

--- WizardRoyale/src/GameElements/Projectile.java
+++ WizardRoyale/src/GameElements/Projectile.java
@@ -1,5 +1,6 @@
 package GameElements;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import Main.Handler;
@@ -8,29 +9,34 @@ import Main.ID;
 public class Projectile extends GameObject{
 	
 	private String type;
-	private int mouseX, mouseY;
+	private double angle;
 	private Handler handler;
+	private boolean isOutOfRange;
 
 
 	
 	public Projectile(String t, int x, int y, ID id, int mouseX, int mouseY, Handler h) {
 		super(x, y, id);
 		type = t;
-		this.mouseX = mouseX;
-		this.mouseY = mouseY;
+		angle = Math.atan((y - mouseY) / (mouseX - x)); //Not sure if this works yet
 		handler = h;
+		isOutOfRange = false;
 	}
 
 	@Override
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
-		
+		g.setColor(Color.red);
+		g.fillRect(xCoord, yCoord, 50, 10);
 	}
 
 	@Override
 	public void tick() {
 		// TODO Auto-generated method stub
-		
+		xCoord += Math.cos(angle) * 5;
+		yCoord += Math.sin(angle) * 5;
+		if(isOutOfRange)
+			handler.getGameObjects().remove(this);
 		
 	}
 	public String getType() {

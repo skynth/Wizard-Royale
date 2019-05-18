d81e0c61627f082179c8849fd2b34160d10d1901
Leofeng <Leofeng@192.168.0.11>
2019-05-17 19:05:59 -0700

    s

--- WizardRoyale/src/Main/WizardRoyale.java
+++ WizardRoyale/src/Main/WizardRoyale.java
@@ -324,16 +324,11 @@ public class WizardRoyale extends Canvas implements Runnable, NetworkListener {
 				for (int i = 0; i < handler.getPlayers().size(); i++) {
 					
 					if (handler.getPlayers().get(i).getIp().equals(host)) {
-<<<<<<< HEAD
-						System.out.print("success");
-						handler.getPlayers().get(i).setVelX((float)ndo.message[1]);
-						handler.getPlayers().get(i).setVelY((float)ndo.message[2]);
-=======
+
 						System.out.print("success");
 						handler.getPlayers().get(i).setVelX((float)ndo.message[1]);
 						handler.getPlayers().get(i).setVelY((float)ndo.message[2]);
 
->>>>>>> branch 'master' of https://github.com/Leofeng1/APCS-Final.git
 					}
 				}
 			}

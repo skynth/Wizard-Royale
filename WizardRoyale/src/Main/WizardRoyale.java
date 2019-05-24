6b0148dc02f7122e6a98cb2968dee5c17f19716d
lfeng014 <lfeng014@PR18.hhs.win.fuhsd.org>
2019-05-24 12:43:43 -0700

    x

--- WizardRoyale/src/Main/WizardRoyale.java
+++ WizardRoyale/src/Main/WizardRoyale.java
@@ -401,7 +401,7 @@ public class WizardRoyale extends Canvas implements Runnable, NetworkListener {
 				for (int i = 0; i < handler.getPlayers().size(); i++) {
 
 					if (handler.getPlayers().get(i).getIp().equals(host)) {
-						/*
+						
 						System.out.println("success");
 						handler.getPlayers().get(i).setRight((boolean)ndo.message[1]);
 						
@@ -417,8 +417,8 @@ public class WizardRoyale extends Canvas implements Runnable, NetworkListener {
 						
 						handler.getPlayers().get(i).setUp((boolean)ndo.message[3]);
 						handler.getPlayers().get(i).setDown((boolean)ndo.message[4]);
-						*/
-						if((int)ndo.message[1] > handler.getPlayers().get(i).getX()) {
+						
+						/*if((int)ndo.message[1] > handler.getPlayers().get(i).getX()) {
 								handler.getPlayers().get(i).setAnimationRight(true);
 						}
 						else if((int)ndo.message[1] < handler.getPlayers().get(i).getX()) {
@@ -427,7 +427,7 @@ public class WizardRoyale extends Canvas implements Runnable, NetworkListener {
 						}
 						handler.getPlayers().get(i).setX((int)ndo.message[1]);
 						handler.getPlayers().get(i).setY((int)ndo.message[2]);
-						
+						*/
 
 					}
 					
@@ -478,8 +478,8 @@ public class WizardRoyale extends Canvas implements Runnable, NetworkListener {
 					}
 					hasMoveToStart = true;
 				}
-				//nm.sendMessage(NetworkDataObject.MESSAGE, messageTypeMove, myPlayer.isRight(), myPlayer.isLeft(), myPlayer.isUp(), myPlayer.isDown());
-				nm.sendMessage(NetworkDataObject.MESSAGE, messageTypeMove, myPlayer.getX(), myPlayer.getY());
+				nm.sendMessage(NetworkDataObject.MESSAGE, messageTypeMove, myPlayer.isRight(), myPlayer.isLeft(), myPlayer.isUp(), myPlayer.isDown());
+				//nm.sendMessage(NetworkDataObject.MESSAGE, messageTypeMove, myPlayer.getX(), myPlayer.getY());
 
 			
 

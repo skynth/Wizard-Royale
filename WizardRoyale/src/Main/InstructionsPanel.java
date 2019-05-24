6bdc1b44a68fd0648fffbe01f5834dc526c5f15e
Leofeng <Leofeng@192.168.0.11>
2019-05-24 00:15:51 -0700

    instructions

--- WizardRoyale/src/Main/InstructionsPanel.java
+++ WizardRoyale/src/Main/InstructionsPanel.java
@@ -1,10 +1,12 @@
 package Main;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
@@ -12,13 +14,18 @@ import java.awt.Toolkit;
 /**
  * The panel that will hold the graphics for the instructions to the game. Will appear when the user clicks the "help" button in the main menu panel
  * @author Leofeng 
- * @version 5/6/19
+ * @version 5/24/19
  *
  */
 
 public class InstructionsPanel {
 	
 	Image background = Toolkit.getDefaultToolkit().getImage("Resources" + MainMenuPanel.FILE_SEP + "Instructions_background.jpg");	
+	
+	private Image medKitImage = Toolkit.getDefaultToolkit().getImage("Resources" + MainMenuPanel.FILE_SEP + "medicalKit.png");
+	private Image fireImage = Toolkit.getDefaultToolkit().getImage("Resources" + MainMenuPanel.FILE_SEP + "fireConsumable.png");
+	private Image armorImage = Toolkit.getDefaultToolkit().getImage("Resources" + MainMenuPanel.FILE_SEP + "cloak3.png");
+	
 	/**
 	 * Method that draws the graphics for the instructions to the game 
 	 * @param g the instance of the graphics class that will handle drawing everything in the WizardRoyale class
@@ -40,6 +47,28 @@ public class InstructionsPanel {
 	    
 	    g.setColor(Color.white);
 	    g.drawString("INSTRUCTIONS", x, y);
+	    
+	    Graphics2D g2d = (Graphics2D) g;
+	    g2d.setStroke(new BasicStroke(10));
+	    g2d.drawLine(WizardRoyale.WIDTH / 2, 150, WizardRoyale.WIDTH / 2, WizardRoyale.HEIGHT - 100);
+	    
+	    
+	    g.setColor(Color.green);
+	  	g.drawString("Press WASD To Move", WizardRoyale.WIDTH / 25, WizardRoyale.HEIGHT / 5);
+	  	g.drawString("Click the Mouse to Shoot", WizardRoyale.WIDTH / 25, WizardRoyale.HEIGHT / 3);
+	  	
+	  	font0 = new Font("arial", Font.BOLD, (int)(WizardRoyale.WIDTH / 34));
+	  	g.setFont(font0);
+	  	g.drawString("Walk over Items to pick them up", WizardRoyale.WIDTH / 25, (int) (WizardRoyale.HEIGHT / 2.15));
+	  	
+	  	g.drawImage(medKitImage, WizardRoyale.WIDTH / 2 + WizardRoyale.WIDTH / 23, (int) (WizardRoyale.HEIGHT / 5.2), (int)(WizardRoyale.WIDTH / 30), (int)(WizardRoyale.HEIGHT / 25), null);
+	  	g.drawString("Restores 30 HP", WizardRoyale.WIDTH / 2 + (int) (WizardRoyale.WIDTH / 12.5), (int) (WizardRoyale.HEIGHT / 4.5));
+	  	
+	  	g.drawImage(armorImage, WizardRoyale.WIDTH / 2 + WizardRoyale.WIDTH / 23, (int) (WizardRoyale.HEIGHT / 3.8), (int)(WizardRoyale.WIDTH / 30), (int)(WizardRoyale.HEIGHT / 25), null);
+	  	g.drawString("Gives 30 Armor", WizardRoyale.WIDTH / 2 + (int) (WizardRoyale.WIDTH / 12.5), (int) (WizardRoyale.HEIGHT / 3.3));
+	  	
+	  	g.drawImage(fireImage, WizardRoyale.WIDTH / 2 + WizardRoyale.WIDTH / 23, (int) (WizardRoyale.HEIGHT / 2.9), (int)(WizardRoyale.WIDTH / 30), (int)(WizardRoyale.HEIGHT / 25), null);
+	  	g.drawString("", WizardRoyale.WIDTH / 2 + (int) (WizardRoyale.WIDTH / 12.5), (int) (WizardRoyale.HEIGHT / 4.2));
 	    
 		
 	}

fd0dd32305b867580ebb649c59d39bcaeec17786
lfeng014 <lfeng014@LB13.hhs.win.fuhsd.org>
2019-05-24 13:21:27 -0700

    x

--- WizardRoyale/src/Main/BufferedImageLoader.java
+++ WizardRoyale/src/Main/BufferedImageLoader.java
@@ -1,6 +1,13 @@
+
+
 package Main;
 
 import java.awt.image.BufferedImage;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * @author Leofeng
@@ -10,14 +17,14 @@ import java.awt.image.BufferedImage;
  * 
  */
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 public class BufferedImageLoader {
 	
 	private BufferedImage image;
+	
+	/**
+	 * @param path the path of the file to be converted to a BufferImage
+	 * @return The BufferedImage of the file
+	 */
 	
 	public BufferedImage loadImage(String path) {
 		try {
